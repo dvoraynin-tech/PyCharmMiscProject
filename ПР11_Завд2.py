78e1d5881941c12986b3cd3d34d8650119f12ab1
dvoraynin-tech <dvoraynin@gmail.com>
2025-11-18 12:26:22 +0200

    .

--- ПР11_Завд2.py
+++ ПР11_Завд2.py
@@ -26,22 +26,25 @@ print(total_by_lane)
 
 df["Date"] = pd.to_datetime(df["Date"], dayfirst=True)
 
-df["Month"] = df["Date"].dt.month
+df["Month"] = df["Date"].dt.month_name(locale="uk_UA")
 
 lanes = ["Berri1", "Maisonneuve_3", "Rachel / Papineau"]
 
 print("\nНайпопулярніший місяць на 3 вибраних велодоріжках:")
 for lane in lanes:
-    monthly = df.groupby("Month")[lane].sum()
-    best_month = monthly.idxmax()
-    print(f"{lane}: найпопулярніший місяць — {best_month}")
+    monthly = df.groupby(df["Date"].dt.month)[lane].sum()
+    best_month_num = monthly.idxmax()
+    best_month_name = pd.to_datetime(str(best_month_num), format="%m").month_name(locale="uk_UA")
+    print(f"{lane}: найпопулярніший місяць — {best_month_name}")
 
 lane_to_plot = "Berri1"
 
-monthly_counts = df.groupby("Month")[lane_to_plot].sum()
+monthly_counts = df.groupby(df["Date"].dt.month)[lane_to_plot].sum()
+
+month_names = pd.to_datetime(monthly_counts.index, format="%m").month_name(locale="uk_UA")
 
 plt.figure(figsize=(10, 5))
-plt.plot(monthly_counts.index, monthly_counts.values)
+plt.plot(month_names, monthly_counts.values)
 
 plt.title(f"Завантаженість велодоріжки {lane_to_plot} по місяцях у 2017р.")
 plt.xlabel("Місяць")
